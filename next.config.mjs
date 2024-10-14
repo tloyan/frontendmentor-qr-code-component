24b6178dc35dba793ac0490a54ef4e037c6a4826
tloyan <thomas.loyan@gmail.com>
2024-10-14 21:13:54 +0200

    update remotePatterns to domain

--- next.config.mjs
+++ next.config.mjs
@@ -2,15 +2,16 @@
 const nextConfig = {
     assetPrefix: 'https://qr-code-component.tloyan.com',
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'qr-code-component.tloyan.com',
-            pathname: '/**'
-          },
-        ],
-      },
-  };
+    //     remotePatterns: [
+    //       {
+    //         protocol: 'https',
+    //         hostname: 'qr-code-component.tloyan.com',
+    //         pathname: '/**'
+    //       },
+    //     ],
+        domains: ['qr-code-component.tloyan.com']
+    },
+}
   
 
 export default nextConfig;
