2faaca1f00ce44f747d0f47f43b0dcef2aae0ddf
tloyan <thomas.loyan@gmail.com>
2024-10-14 18:43:26 +0200

    add image domaine

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    assetPrefix: 'https://qr-code-component.tloyan.com'
+    assetPrefix: 'https://qr-code-component.tloyan.com',
+    images: {
+        domains: ['qr-code-component.tloyan.com'],
+    },
   };
   
 
