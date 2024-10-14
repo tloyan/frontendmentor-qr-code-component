6e2fb3cb503b1044c87ca4e034fdb7239bf8c2be
tloyan <thomas.loyan@gmail.com>
2024-10-14 15:23:20 +0200

    add assetPrefix in order to fix assets break in prod

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    assetPrefix: 'https://www.tloyan.com/challenges/qr-code-component'
+};
 
 export default nextConfig;
