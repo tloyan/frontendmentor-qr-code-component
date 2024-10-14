b99616afb23297469ad4dd57fab999d941870ced
tloyan <thomas.loyan@gmail.com>
2024-10-14 21:04:24 +0200

    update remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const nextConfig = {
           {
             protocol: 'https',
             hostname: 'qr-code-component.tloyan.com',
+            pathname: '/**'
           },
         ],
       },
